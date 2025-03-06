b366617b12286791195adfa8d6c4a46d4dbe5b04
Nate <8bitsodagamer@gmail.com>
2025-03-07 02:23:03 +0400

    first and the last

--- OOP_1/OOP_1.cpp
+++ OOP_1/OOP_1.cpp
@@ -1,20 +1,61 @@
-// OOP_1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+﻿//Требования:
+//Закрытые(private) поля :
+//    brand – марка автомобиля.
+//    model – модель автомобиля.
+//    year – год выпуска.
+//    mileage – пробег.
+//    Открытые(public) методы :
+//    Конструктор : принимает значения марки, модели, года выпуска и пробега.
+//    Default Конструктор : инициализирует данные старндартные
+//    Геттеры :
+//getMileage() – возвращает пробег.
+//Сеттер :
+//    setMileage(newMileage) – устанавливает пробег(нельзя уменьшать пробег).
+//    Метод drive(km) : увеличивает пробег на переданное количество километров.
+//    Метод getAge(currentYear) : возвращает возраст автомобиля.
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+
+class Car
+{
+    char _brand[20];
+    char _model[20];
+    unsigned short _year;
+    unsigned int _mileage;
+
+public:
+
+    Car() :_brand("Unkown"), _model("Unknown"), _year(0), _mileage(0) {}
+
+    Car(const char brand[], const char model[], const unsigned short year, const unsigned int mileage)
+    {
+        strncpy(_brand, brand, sizeof(_brand));
+        strncpy(_model, model, sizeof(_model));
+        _year = year;
+        _mileage = mileage;
+    };
+
+    unsigned int get_mileage() { return _mileage; }
+
+    unsigned short get_age(unsigned short current_year) { return current_year > _year ? current_year - _year : 0; }
+
+    void set_mileage(const unsigned int mileage){ _mileage = mileage > _mileage ? mileage : _mileage; }
+
+    void drive(const unsigned int km) { _mileage += km; }
+
+    //void display_car(const Car& car)
+    //{
+    //    std::cout << "Brand: " << _brand << "\n";
+    //    std::cout << "Model: " << _model << "\n";
+    //    std::cout << "Year: " << _year << "\n";
+    //    std::cout << "Mileage: " << _mileage << "\n";
+    //}
+
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Car car("some_brand", "some_model", 2001, 1200);
+    car.display_car(car);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
